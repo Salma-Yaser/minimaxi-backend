fd8fab16b38168c20b51bedf8487a176de094256
Salma-Yaser <01030845657ss@gmail.com>
2026-05-02 03:39:10 +0300

    remove gmail smtp use sendgrid only

--- src/main/java/com/minimaxi/backend/service/EmailService.java
+++ src/main/java/com/minimaxi/backend/service/EmailService.java
@@ -1,5 +1,9 @@
+package com.minimaxi.backend.service;
 
 import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
